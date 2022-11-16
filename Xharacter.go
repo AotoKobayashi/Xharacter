ea23852d88df426d61aaae2c4e8f34fe50a65527
Aoto Kobayashi <kabayashi.san1992@proton.me>
2022-11-16 00:44:23 -0300

    initial commit

---
+++ Xharacter.go
@@ -0,0 +1,26 @@
+package Xharacter
+
+func AlphabetMi() []string {
+	return []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+		"o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+}
+
+func AlphabetMa() []string {
+	return []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+		"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+}
+
+func Alphabet() []string {
+	return []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+		"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+		"o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+}
+
+func SpecialCharacters() []string {
+	return []string{"!", "@", "$", "%", "¨", "&", "*", "(", ")", "-", "_", "=", "+", "'", "`", "´",
+		"[", "{", "}", "]", "^", "~", "<", ",", ".", ">", ":", ";", "/", "?", "|"}
+}
+
+func Numbers() []string {
+	return []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AotoKobayashi/Xharacter
+
+go 1.19
